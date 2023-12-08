f5834e8f8c0305828e8203bd2d77f6e11cc41a23
Ahmed <bhaia9036@gmail.com>
2023-12-08 07:19:40 -0800

    Initial Commit

---
+++ Models/User.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose';
+const mongodbURI = process.env.mongodbURI || "mongodb+srv://ahmedradiantcortex:ahmedradiantcortex@cluster0.sv2mcyd.mongodb.net/";
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+});
+
+
+
+const productSchema = new mongoose.Schema({
+projectName:{ type: String },
+amountRequired:  { type: String },
+collectedAmount:  { type: String }, 
+amountRemaining : { type: String }, 
+projectDescription:  { type: String }, 
+imageUrl: { type: String  ,  required: true },
+createdOn: { type: Date, default: Date.now },
+});
+export const tweetModel = mongoose.model('ProductsAll', productSchema);
+  
+
+
+
+
+const productSchema2 = new mongoose.Schema({
+    projectName:{ type: String },
+    projectDescription:  { type: String }, 
+    imageUrl: { type: String  ,  required: true },
+    createdOn: { type: Date, default: Date.now },
+    });
+export const tweetModel2 = mongoose.model('ProductsAll2', productSchema2);
+
+
+
+
+const User = mongoose.model('User', userSchema);
+mongoose.connect(mongodbURI);
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+mongoose.connection.on('connected', function () {//connected
+    console.log("Mongoose is connected");
+});
+
+mongoose.connection.on('disconnected', function () {//disconnected
+    console.log("Mongoose is disconnected");
+    process.exit(1);
+});
+
+mongoose.connection.on('error', function (err) {//any error
+    console.log('Mongoose connection error: ', err);
+    process.exit(1);
+});
+
+process.on('SIGINT', function () {/////this function will run jst before app is closing
+    console.log("app is terminating");
+    mongoose.connection.close(function () {
+        console.log('Mongoose default connection closed');
+        process.exit(0);
+    });
+});
+////////////////mongodb connected disconnected events///////////////////////////////////////////////
+
+export default User;
